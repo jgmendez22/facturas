4ab9700e2b7545fac28469cc0fc764c2530c0847
jgmendez22 <96554469+jgmendez22@users.noreply.github.com>
2022-11-24 11:22:54 -0500

    Update V4__detail.sql

--- src/main/resources/db/migration/V4__detail.sql
+++ src/main/resources/db/migration/V4__detail.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS detail(
     id SERIAL,
-    quantity DECIMAL,
+    quantity FLOAT (5,4),
     FOREIGN KEY(product_id) REFERENCES product.id
     FOREIGN KEY(invoice_id) REFERENCES invoice.id
     PRIMARY KEY (id),
